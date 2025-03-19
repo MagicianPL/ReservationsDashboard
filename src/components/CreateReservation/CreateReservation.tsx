05a28197b368e4ec09c0027fda69b2c422b5b860
Michał <sukowskimichal@gmail.com>
2025-03-19 17:42:52 +0100

    routing and new page added

---
+++ src/components/CreateReservation/CreateReservation.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import '../../App.css';
+
+const CreateReservation: React.FC = () => {
+    return (
+        <div className="app-container">
+            <Header />
+            <main className="main-content">
+                <h1>New Page</h1>
+            </main>
+        </div>
+    );
+};
+
+export default CreateReservation; 
